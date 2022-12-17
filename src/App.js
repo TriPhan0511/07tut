dd5a55421fa6de65ea5f2ab69d6068f3d129dec1
Tri Phan <phanphutri@gmail.com>
2022-12-17 16:11:44 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import AddItem from './AddItem'
 import Content from './Content'
 import Footer from './Footer'
 import Header from './Header'
@@ -21,6 +22,7 @@ function App() {
       item: 'Item 3',
     },
   ])
+  const [newItem, setNewItem] = useState('')
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
@@ -36,9 +38,14 @@ function App() {
     localStorage.setItem('shoppinglist', JSON.stringify(listItems))
   }
 
+  const handleSubmit = (e) => {
+    console.log('submitted')
+  }
+
   return (
     <div className='App'>
       <Header title='Grocery List' />
+      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
       <Content
         items={items}
         setItems={setItems}
@@ -51,3 +58,50 @@ function App() {
 }
 
 export default App
+
+// import { useState } from 'react'
+// import AddItem from './AddItem'
+// import Content from './Content'
+// import Footer from './Footer'
+// import Header from './Header'
+
+// function App() {
+//   const data = JSON.parse(localStorage.getItem('shoppinglist')) ?? []
+//   const [items, setItems] = useState(data)
+
+//   const handleCheck = (id) => {
+//     const listItems = items.map((item) =>
+//       item.id === id ? { ...item, checked: !item.checked } : item
+//     )
+//     setItems(listItems)
+//     localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+//   }
+
+//   const handleDelete = (id) => {
+//     const listItems = items.filter((item) => item.id !== id)
+//     setItems(listItems)
+//     localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+//   }
+
+//   const addShoppingItem = (item) => {
+//     const listItems = [...items, item]
+//     setItems(listItems)
+//     localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+//   }
+
+//   return (
+//     <div className='App'>
+//       <Header title='Grocery List' />
+//       <AddItem addShoppingItem={addShoppingItem} />
+//       <Content
+//         items={items}
+//         setItems={setItems}
+//         handleCheck={handleCheck}
+//         handleDelete={handleDelete}
+//       />
+//       <Footer length={items?.length ?? 0} />
+//     </div>
+//   )
+// }
+
+// export default App
